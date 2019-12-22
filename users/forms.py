793f4a7049413d5fcdf8c368314c3e485120d962
voycetony <voycetony@yahoo.com>
2019-12-22 18:18:04 -0500

    first commit

---
+++ users/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+class CustomUserCreationForm(UserCreationForm):
+   class Meta(UserCreationForm.Meta):
+      model = CustomUser
+      fields = fields = ('username', 'email', 'age',)
+class CustomUserChangeForm(UserChangeForm):
+   class Meta:
+      model = CustomUser
+      fields = ('username', 'email', 'age',)
