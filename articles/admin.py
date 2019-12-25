7e03af1642a20579b0df09ca53c45b14a37c1c2a
voycetony <voycetony@yahoo.com>
2019-12-25 12:21:17 -0500

    Added permissions and comments

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-from .models import Article
-admin.site.register(Article)
+from .models import Article, Comment
+class CommentInline(admin.TabularInline): # new
+   model = Comment
+class ArticleAdmin(admin.ModelAdmin):
+   inlines = [
+      CommentInline,
+   ]
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)  
